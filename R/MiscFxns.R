101addf72c45e00761bb06a70bc83baa0f820e65
Sarah Popov <sarah.popov@gmail.com>
2022-02-24 13:59:40 -0800

    Add change_yaml_matter function.

--- R/MiscFxns.R
+++ R/MiscFxns.R
@@ -354,3 +354,56 @@ bbox_at_zoom <- function(locs, zoom_level = 7) {
 
   return(bb)
 }
+
+# -----
+
+#' Change YAML header content
+#' @description This function updates the YAML content of a given Rmd file.
+#' YAML content is passed as a list via the dots argument of the function.
+#' @input_file Input .Rmd file with YAML to be updated
+#' @output_file Output .Rmd filename with updated YAML (optional; if left blank, updated .Rmd file will be printed in the console.)
+#' @references Code from: https://stackoverflow.com/a/66908611/1454785
+#' @returns A .Rmd file with updated YAML content, either saved to the `output_file` path location or printed in the console.
+#' @export
+
+change_yaml_matter <- function(input_file, ..., output_file) {
+  input_lines <- readLines(input_file)
+  delimiters <- grep("^---\\s*$", input_lines)
+  if (!length(delimiters)) {
+    stop("unable to find yaml delimiters")
+  } else if (length(delimiters) == 1L) {
+    if (delimiters[1] == 1L) {
+      stop("cannot find second delimiter, first is on line 1")
+    } else {
+      # found just one set, assume it is *closing* the yaml matter;
+      # fake a preceding line of delimiter
+      delimiters <- c(0L, delimiters[1])
+    }
+  }
+  delimiters <- delimiters[1:2]
+  yaml_list <- yaml::yaml.load(
+    input_lines[ (delimiters[1]+1):(delimiters[2]-1) ])
+
+  dots <- list(...)
+
+  for (element_name in names(dots)){
+    if(element_name %in% names(yaml_list)) {
+      yaml_list[element_name] <- dots[element_name]
+    } else {
+      yaml_list <- c(yaml_list,dots[element_name])
+    }
+  }
+
+  output_lines <- c(
+    if (delimiters[1] > 0) input_lines[1:(delimiters[1])],
+    strsplit(yaml::as.yaml(yaml_list), "\n")[[1]],
+    input_lines[ -(1:(delimiters[2]-1)) ]
+  )
+
+  if (missing(output_file)) {
+    return(output_lines)
+  } else {
+    writeLines(output_lines, con = output_file)
+    return(invisible(output_lines))
+  }
+}
