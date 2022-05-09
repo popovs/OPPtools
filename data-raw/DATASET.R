4500f4987f271d3da5e51df83cfb8bace8486931
allisonglider <allisonglider@gmail.com>
2022-05-09 10:23:14 -0700

    Add opp_mb_projects to package

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -11,3 +11,9 @@ usethis::use_data(murres, overwrite = T)
 cpf_report_params <- read.csv('data-raw/opp-cpf-report-params.csv', stringsAsFactors = F)
 
 usethis::use_data(cpf_report_params, overwrite = T)
+
+# read in a table with list of mb projects associated with OPP
+
+opp_mb_projects <- read.csv('data-raw/opp-mb-projects.csv', stringsAsFactors = F)
+
+usethis::use_data(opp_mb_projects, overwrite = T)
